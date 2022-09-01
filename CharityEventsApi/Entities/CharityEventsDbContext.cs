3f955adbc05b0eaf45d1a1bf9bdcb9e557c064da
Ktosi33 <54289137+Ktosi33@users.noreply.github.com>
2022-09-01 17:08:09 +0200

    dbcontext

---
+++ CharityEventsApi/Entities/CharityEventsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CharityEventsApi.Entities
+{
+    public class CharityEventsDbContext : DbContext
+    {
+        private string _connectionString =
+              "Server=localhost;Port=3306;Database=CharityEventsDb;Username=root;Password=";
+
+      
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+                 => optionsBuilder.UseMySql(_connectionString, ServerVersion.AutoDetect(_connectionString));
+    }
+}
