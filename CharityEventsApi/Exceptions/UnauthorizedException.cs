40bcb8b142a9a1c68c0d168c79cd4fb0ec925937
Ktosi33 <54289137+Ktosi33@users.noreply.github.com>
2022-12-07 21:21:39 +0100

    UnauthorizedException
    +Added UnauthorizedException

---
+++ CharityEventsApi/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,16 @@
+﻿namespace CharityEventsApi.Exceptions
+{
+    public class UnauthorizedException : Exception
+    {
+        public const int StatusCode = 401;
+        public UnauthorizedException(string message) : base(message)
+        {
+
+        }
+        public UnauthorizedException() : base("Unauthorized exception, status code 401.") { }
+        {
+
+        }
+       
+    }
+}
